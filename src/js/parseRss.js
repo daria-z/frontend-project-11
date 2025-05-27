b86d1654fdf4a1f7fc5f8c96e11f6a951fd3548f
Daria Zakirova <daria.zakirova@gmail.com>
2025-05-27 20:29:43 +0300

    исправлен парсинг данных

--- src/js/parseRss.js
+++ src/js/parseRss.js
@@ -1,27 +1,27 @@
-export const parseRss = (xmlSting) => {
+export const parseRss = (xmlString) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(xmlSting, 'application/xml');
+  const doc = parser.parseFromString(xmlString, "application/xml");
 
   const error = doc.querySelector("parsererror");
   if (error) {
-    throw new Error('ошибка парсинга xml');
+    throw new Error("ошибка парсинга xml");
   }
 
   const channel = {
+    id: 1, // добавить
     title: doc.querySelector("channel > title")?.textContent || "",
     description: doc.querySelector("channel > description")?.textContent || "",
+    link: doc.querySelector("channel > link")?.textContent || "",
+    pubDate: doc.querySelector("channel > pubDate")?.textContent || "",
   };
 
   const items = Array.from(doc.querySelectorAll("item")).map((item) => ({
+    id: 2, //добавить
     title: item.querySelector("title")?.textContent || "",
     description: item.querySelector("description")?.textContent || "",
     link: item.querySelector("link")?.textContent || "",
+    pubDate: item.querySelector("pubDate")?.textContent || "",
   }));
 
-  console.log(channel, items);
+  return { channel, items };
 };
-
-
-
-
-
