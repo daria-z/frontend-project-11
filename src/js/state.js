abe27a253489cdb66cf98a3cd9207614220bd859
Daria Zakirova <daria.zakirova@gmail.com>
2025-05-27 09:51:52 +0300

    обновлена структура state

--- src/js/state.js
+++ src/js/state.js
@@ -3,15 +3,45 @@ import { renderErrors, renderInputValue } from "./view.js";
 
 const createState = () => {
   const object = {
-    inputValue: "",
-    rssFeed: [],
-    errors: null,
+    form: {
+      inputValue: "",
+      errors: ""
+    },
+    feed: {
+      channel: {
+        title: "",
+        description: "",
+        link: "",
+      },
+      items: [],
+      activeItem: null,
+    },
+    ui: "",
   };
+
+  // items: [
+  //   {
+  //     id: "1",
+  //     title: "Post 1",
+  //     description: "Description 1",
+  //     link: "https://example.com/post1",
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "Post 2",
+  //     description: "Description 2",
+  //     link: "https://example.com/post2",
+  //   },
+  // ],
 
   const state = onChange(object, (path, value) => {
     console.log(`состояние изменено: ${path}`, value);
-    if (path === "rssFeed") {
+    if (path === "feed") {
       console.log("rss feed:", value);
+    }
+    if (path === "activeItem") {
+      // поиск по массиву постов
+      // рендер модалки
     }
     if (path === "inputValue") {
       renderInputValue(value);
