d8f40b1a92d95241c9fd5abb319367a87f06ce61
Daria Zakirova <daria.zakirova@gmail.com>
2025-06-16 21:27:32 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
-import path from "path";
+// import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname, "src"),
+  // root: path.resolve(__dirname, "/"),
+  // build: {
+  //   outDir: 'build',
+  //   assetsDir: 'assets'
+  // },
   server: {
     port: 8080,
     hot: true,
