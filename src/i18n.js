17a2be7d8e8de6749281fb91db78d2acf4763323
Daria Zakirova <daria.zakirova@gmail.com>
2025-06-16 22:14:00 +0300

    fix: добавлен дефолтный язык

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import translations from "./locales/index.js";
 i18next.init({
+  lng: "ru",
   resources: translations,
   fallbackLng: "ru",
   supportedLngs: ["en", "ru"],
